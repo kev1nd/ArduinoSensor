e2a866154eef06e67b7d7c5a184be8af743d1de4
Kevin Dunwell <kevin@dunwell.org>
2018-02-04 12:04:04 +0000

    working but has timeout errors

--- websend.ino
+++ websend.ino
@@ -10,7 +10,9 @@
 //  02/03/18  1.0.2   Changed SSID to VioletInternet for new hub
 //  02/03/18  1.0.3   Add support for DHT11. Add support for Photo-resistor
 
+#define _ESPLOGLEVEL_ 4
 #include <WiFiEsp.h>
+
 #include <SimpleDHT.h>
 // for DHT11,
 //      VCC: 5V or 3V
@@ -28,8 +30,8 @@ SoftwareSerial Serial1(6, 7); // RX, TX
 
 unsigned long waitTime = 30000; // Time to wait for a response
 char ssid[] = "VioletInternet"; // Wifi SSID
-char pass[] = "kjjj1997";     // Wifi Password
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
+char pass[] = "kjjj1997";       // Wifi Password
+int status = WL_IDLE_STATUS;    // the Wifi radio's status
 char server[] = "api.thingspeak.com";
 int tempPin = 0;                // Sensor pin for Thermister
 int lightPin = 1;               // Sensor pin for light level
@@ -63,11 +65,15 @@ void setup()
 
 
   digitalWrite(LED_BUILTIN, LOW);
+
+  printWifiStatus();
+
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH);
+
 
   int lightLevel  = analogRead(lightPin);
 
@@ -79,36 +85,90 @@ void loop()
     return;
   }
 
-  String thermister = (String)getThermister();
-
-  sendData(thermister, (String)temperature, (String)humidity, (String)lightLevel);
+  float therm = getThermister();
+  sendDataToThingSpeak(therm, temperature, humidity, lightLevel);
 
   digitalWrite(LED_BUILTIN, LOW);
 
   delay(30000);
 
+  if (client.available()) {
+    String resp = "";
+    while (client.available())
+    {
+      resp += (char)client.read();
+    }
+    Serial.println("Remote response: " + resp);
+  }
 }
 
 
 
-void sendData(String field1, String field2, String field3, String field4) {
+void sendDataToThingSpeak(float field1, int field2, int field3, int field4) {
+  int cCount = 0;
+
+  char field1str[10];
+
+  /* 4 is mininum width, 2 is precision; float value is copied onto str_temp*/
+  dtostrf(field1, 4, 2, field1str);
+
+  char myCommand[90];
+  sprintf(myCommand, "GET /update?api_key=WZDTWH6PEE2G0YII&field1=%s&field2=%i&field3=%i&field4=%i", field1str, field2, field3, field4);
+  Serial.println(myCommand);
+
 
   client.stop();
-  if (client.connect(server, 80)) {
-    Serial.println("Connecting...");
+  if (client.connectSSL(server, 443)) {
+    //Serial.println("Connecting...");
 
     // send the HTTP PUT request
-    client.println("GET https://api.thingspeak.com/update?api_key=WZDTWH6PEE2G0YII&field1=" + field1 + "&field2=" + field2 + "&field3=" + field3 + "&field4=" + field4);
-    //client.println("Host: api.thingspeak.com");
-    client.println("Connection: close");
+    cCount = client.println(myCommand);
+    //  Serial.println(cCount);
+    cCount = client.println(F("Host: api.thingspeak.com"));
+    //  Serial.println(cCount);
+    // cCount = client.println(F("Connection: close"));
+    // Serial.println(cCount);
     client.println();
   }
   else {
     // if you couldn't make a connection
     Serial.println("Connection failed");
   }
+  Serial.print("Characters written = ");
+  Serial.println(cCount);
+
+  
+  delay(1000);
+  if (client.available()) {
+    char* resp = "";
+    while (client.available())
+    {
+      resp += (char)client.read();
+    }
+    Serial.print("Remote response:");
+    Serial.println(resp);
+  }
+
 }
 
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
 
 
 float getThermister() {
